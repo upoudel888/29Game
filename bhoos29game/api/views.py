a429c1515c40fe3ea28358bbed93b6d5a12a7ffd
upoudel888 <upoudel888@gmail.com>
2022-12-23 09:43:20 +0545

    created utils.py with incoming data format

--- bhoos29game/api/views.py
+++ bhoos29game/api/views.py
@@ -6,32 +6,30 @@ from django.http import  HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from . import utils
 
 
+def home(request):
+    return HttpResponse("This is the home page")
 
 @csrf_exempt
 @api_view(['GET'])
 def hi(request):
-    print("Sending back hello")
-    response_data =json.dumps({"value":"hello"})
     return JsonResponse({"value":"hello"})
-
-def home(request):
-    return HttpResponse("This is the home page")
 
 
 @csrf_exempt
 @api_view(['POST','GET'])
 def bid(request):
-    print(request.data)
-    return JsonResponse({"bid":17})
+    bid_response = utils.predictBidValue(request.data)
+    return JsonResponse(bid_response)
 
 @api_view(['POST','GET'])
 def chooseTrump(request):
-    print(request.data)
-    return JsonResponse({"suit":'H'})
+    trump_choice = utils.predictTrump(request.data)
+    return JsonResponse(trump_choice)
 
 @api_view(['POST','GET'])
 def play(request):
-    print(request.data)
-    return JsonResponse({"card": "JS"})
+    play_response = utils.predictPlay(request.data)
+    return JsonResponse(play_response)
